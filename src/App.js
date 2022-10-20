e52531092a5b6c76bd3fc0885e3cf37d677b6d73
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-10-20 12:35:39 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;

d9b0dede7173e8026aaa5b23bc675c3746ca2c86
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-10-23 12:02:14 +0300

    08-Использование Данных Redux в React Компонентах

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import Counter from "./components/Counter";
-import Header from "./components/Header";
-import Auth from "./components/Auth";
-import UserProfile from "./components/UserProfile";
 
 function App() {
   return (

9a63f4f659b4c5d562d16cc8bb80dc3eee883900
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-10-24 16:19:00 +0300

    15-State Slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,26 @@
 import { legacy_createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, isCounterInvisible: false };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.number;
+    },
+    setCounterVisibility(state) {
+      state.isCounterInvisible = !state.isCounterInvisible;
+    },
+  },
+});
 
 const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
